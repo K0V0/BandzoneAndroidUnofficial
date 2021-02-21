8bdda5a2ab7319bf669daae022f2234b0655df4a
Matej Kováč <kovo.mato@gmail.com>
2021-02-21 17:32:57 +0100

    adding new screenshots

---
+++ app/build/generated/source/buildConfig/release/com/kovospace/bandzoneplayerunofficial/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.kovospace.bandzoneplayerunofficial;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.kovospace.bandzoneplayerunofficial";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 6;
+  public static final String VERSION_NAME = "0.4-alpha";
+}
